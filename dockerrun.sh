20a7522abf8286a8dbc9e13aeffc156e3d6eb388
Zimonzk <simon.artner@gmail.com>
2021-01-23 11:28:35 +0100

    anther name update

--- dockerrun.sh
+++ dockerrun.sh
@@ -4,4 +4,4 @@ mkdir -p /opt/murmur
 
 ./build.sh
 
-docker run -i -t -d --name mumble --restart always --network host -v /opt/murmur:/mumble -v /opt/murmur:/bot/config zimonzk/mumbledocker
+docker run -i -t -d --name mumble --restart always --network host -v /opt/murmur:/mumble -v /opt/murmur:/botamusique/config zimonzk/mumbledocker
